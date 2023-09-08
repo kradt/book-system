d6e6d2ebbc7915653edf13a14947befb02a0ab81
krad <mynamedark713@gmail.com>
2023-09-08 18:45:44 +0300

    refactor api for roomss

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Path, status, Depends
+from fastapi import APIRouter, Path, status, Depends, HTTPException
 from typing import Annotated
+from bson import ObjectId
 
 from app.schemas.rooms import Room, RoomUpdate, RoomOutput
 from app.dependencies import get_room
@@ -19,7 +20,6 @@ async def get_specific_room(room_id: Annotated[str, Path(title="The id of specif
 
 @router.patch("/{room_id}", status_code=status.HTTP_200_OK, response_model=RoomOutput)
 async def update_room_info(
-    room_id: Annotated[str, Path(title="The id of specific room")],
     room: RoomUpdate,
     db_room: Annotated[models.Room, Depends(get_room)]):
     """
@@ -29,15 +29,14 @@ async def update_room_info(
     return db_room
 
 
-@router.delete("/{room_id}", status_code=204)
+@router.delete("/{room_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_room(
-        room_id: Annotated[str, Path(title="The id of specific room")],
         db_room: Annotated[models.Room, Depends(get_room)]):
     """
         Remove room from base
     """
     await db_room.delete()
-    return {"message": f"The room with id {room_id} was Succefuly delete"}
+    return {"message": f"The room with id {db_room.id} was Succefuly delete"}
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=Room)
@@ -45,7 +44,9 @@ async def create_new_room(room: Room):
     """
         Create new room
     """
-    new_room = models.Room(name=room.name, columns=room.columns, rows=room.rows, places=room.places)
+    if await models.Room.find_one({"name": room.name}):
+        raise HTTPException(status_code=404, detail="The room with same name already exist")
+    new_room = models.Room(_id=ObjectId(), name=room.name, columns=room.columns, rows=room.rows, places=room.places)
     await new_room.insert()
     return new_room
 
