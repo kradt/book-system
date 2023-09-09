d7bd1a0f669fece2e88985d916de0b178942761b
krad <mynamedark713@gmail.com>
2023-09-09 22:55:17 +0300

    refactor room router

--- app/dependencies.py
+++ app/dependencies.py
@@ -5,6 +5,9 @@ from app import models
 
 
 async def get_room_by_id(room_id):
+    """
+        Depends that useing for getting specific room
+    """
     room = await models.Room.get(room_id)
     if not room:
         raise HTTPException(status_code=404, detail="There is no such room")
@@ -14,6 +17,9 @@ async def get_room_by_id(room_id):
 async def get_seat_by_number(
         db_room: Annotated[models.Room, Depends(get_room_by_id)], 
         seat_number: Annotated[int, Path(title="Number of seat in the room")]):
+    """
+        Depends that using for getting specific seat
+    """
     if seat_number > len(db_room.seats) or seat_number <= 0:
         raise HTTPException(404, "Seat with this number doesn't exist")
     for seat in db_room.seats:
