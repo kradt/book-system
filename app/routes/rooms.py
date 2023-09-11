490981a528511a91598109e1d46bad79285ae40c
krad <mynamedark713@gmail.com>
2023-09-11 18:41:05 +0300

    add tests for patch seat

--- app/routes/rooms.py
+++ app/routes/rooms.py
@@ -67,12 +67,14 @@ async def update_seat_data(
         You can update additional data and booking status of seat
         If you want to change other data you should use patch method of room to change all seats
     """
-    try:
-        if seat.booked == True: db_seat.book()
-        elif seat.booked == False: db_seat.unbook()
-    except ValueError:
-        raise HTTPException(400, "The seat already booked")
-    db_seat.additional_data = seat.additional_data
+    if seat.booked:
+        try:
+            if seat.booked == True: db_seat.book()
+            elif seat.booked == False: db_seat.unbook()
+        except ValueError:
+            raise HTTPException(400, "The seat already booked")
+    if seat.additional_data:
+        db_seat.additional_data = seat.additional_data
     await db_seat.save()
     return db_seat
 
@@ -86,7 +88,7 @@ def get_specific_seat(
     return db_seat
 
 
-@router.get("/rooms/{room_id}/seats/", tags=["Seats"], status_code=200, response_model=Seat)
+@router.get("/rooms/{room_id}/seats/", tags=["Seats"], status_code=200, response_model=list[Seat])
 def get_specific_seat(
         db_room: Annotated[Seat, Depends(get_room_by_id)]):
     """
@@ -95,7 +97,7 @@ def get_specific_seat(
     return db_room.seats
 
 
-@router.patch("/rooms/{room_id}", tags=["Rooms"], status_code=status.HTTP_200_OK, response_model=RoomOutput)
+@router.patch("/rooms/{room_id}/", tags=["Rooms"], status_code=status.HTTP_200_OK, response_model=RoomOutput)
 async def update_room_info(
         room: Room,
         db_room: Annotated[models.Room, Depends(get_room_by_id)]):
@@ -110,7 +112,7 @@ async def update_room_info(
     return db_room
 
 
-@router.delete("/rooms/{room_id}", tags=["Rooms"], status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/rooms/{room_id}/", tags=["Rooms"], status_code=status.HTTP_204_NO_CONTENT)
 async def delete_room(
         db_room: Annotated[models.Room, Depends(get_room_by_id)]):
     """
--- tests/test_room_routes.py
+++ tests/test_room_routes.py
@@ -42,7 +42,7 @@ async def test_delete_specific_room(client, created_room):
     """
         Testing deleting room by it id
     """
-    response = await client.delete(f"/rooms/{created_room.id}")
+    response = await client.delete(f"/rooms/{created_room.id}/")
     assert response.status_code == 204
     assert not await models.Room.find_one({"name": created_room.name})
 
@@ -55,7 +55,7 @@ async def test_patch_query_to_rooms(client, created_room):
     first_json_update = {
         "name": "New name"
     }
-    response = await client.patch(f"/rooms/{created_room.id}", json=first_json_update)
+    response = await client.patch(f"/rooms/{created_room.id}/", json=first_json_update)
     room = response.json()
     assert response.status_code == 200
     assert room["name"] == first_json_update["name"]
@@ -67,11 +67,48 @@ async def test_patch_query_to_rooms(client, created_room):
                 {"row": 1, "column": 2, "number": 2}
             ]
     }
-    response = await client.patch(f"/rooms/{created_room.id}", json=second_json_update)
+    response = await client.patch(f"/rooms/{created_room.id}/", json=second_json_update)
     room = response.json()
     assert response.status_code == 200
     assert room["name"] == first_json_update["name"]
     assert isinstance(room["seats"], list)
     assert len(room["seats"]) == 2
 
-    
+
+@pytest.mark.asyncio
+async def test_get_all_seats_specific_room(client, created_room):
+    response = await client.get(f"/rooms/{created_room.id}/seats/")
+    seats = response.json()
+    assert response.status_code == 200
+    assert isinstance(seats, list)
+    assert created_room.seats[0].row == seats[0]["row"]
+    assert created_room.seats[0].column == seats[0]["column"]
+
+
+@pytest.mark.asyncio
+async def test_get_specific_seat_by_id(client, created_room):
+    response = await client.get(f"/rooms/{created_room.id}/seats/{created_room.seats[0].number}/")
+    seat = response.json()
+    assert response.status_code == 200
+    assert created_room.seats[0].row == seat["row"]
+    assert created_room.seats[0].column == seat["column"]
+
+
+@pytest.mark.asyncio
+async def test_update_specific_seat(client, created_room):
+    first_json_update = {
+        "booked": True,
+    }
+    response = await client.patch(f"/rooms/{created_room.id}/seats/{created_room.seats[0].number}/", json=first_json_update)
+    seat = response.json()
+    assert response.status_code == 200
+    assert seat["booked"] == first_json_update["booked"]
+
+    second_json_update = {
+        "additional_data": {"price": 2500}
+    }
+    response = await client.patch(f"/rooms/{created_room.id}/seats/{created_room.seats[0].number}/", json=second_json_update)
+    seat = response.json()
+    assert response.status_code == 200
+    assert seat["booked"] == first_json_update["booked"]
+    assert seat["additional_data"] == second_json_update["additional_data"]
