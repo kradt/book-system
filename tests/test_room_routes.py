b3d57e896beb165aa9c52d8558464df94b497e49
krad <mynamedark713@gmail.com>
2023-09-14 12:32:39 +0300

    add testing events

--- tests/test_room_routes.py
+++ tests/test_room_routes.py
@@ -127,8 +127,12 @@ async def test_get_all_events_specific_room_by_room_id(client, created_room):
     """
         Test getting all event events of some room
     """
-    pass
-
+    response = await client.get(f"/rooms/{created_room.id}/events/")
+    rooms = response.json()
+    assert response.status_code == 200
+    assert len(rooms) == 1
+    assert created_room.events[0].title == rooms[0]["title"]
+    
 
 @pytest.mark.asyncio
 async def test_create_event_for_specific_room(client, db, created_room):
@@ -153,3 +157,25 @@ async def test_create_event_for_specific_room(client, db, created_room):
     assert response.status_code == 204
     event_in_base = db.query(models.Event).filter_by(title=body["title"]).first()
     assert not event_in_base
+
+
+@pytest.mark.asyncio
+async def test_getting_specific_event_by_it_id(client, db, created_room):
+    """
+        Test getting specific event by id
+    """
+    response = await client.get(f"/events/{created_room.events[0].id}/")
+    event = response.json()
+    assert response.status_code == 200
+    assert event["title"] == created_room.events[0].title
+
+
+@pytest.mark.asyncio
+async def test_deleting_specific_event_by_id(client, db, created_room):
+    """
+        Test deleting specific event by it id
+    """
+    event_id = created_room.events[0].id
+    response = await client.delete(f"/events/{event_id}/")
+    assert response.status_code == 204
+    assert not db.query(models.Event).filter_by(id=event_id).first()
